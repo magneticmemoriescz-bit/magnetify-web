33b056d8f33274c5a7d718d5b57d6f7c1bbc29ad
magneticmemoriescz-bit <magnetic.memories.cz@gmail.com>
2025-11-23 14:54:26 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import { CartProvider } from './context/CartContext';
 import { ProductProvider } from './context/ProductContext';
 import { AppLayout } from './components/layout/AppLayout';
 
 function App() {
   return (
-    <CartProvider>
-      <ProductProvider>
-        <HashRouter>
-          <AppLayout />
-        </HashRouter>
-      </ProductProvider>
-    </CartProvider>
+    <HelmetProvider>
+      <CartProvider>
+        <ProductProvider>
+          <HashRouter>
+            <AppLayout />
+          </HashRouter>
+        </ProductProvider>
+      </CartProvider>
+    </HelmetProvider>
   );
 }
 
