fd2bba05dc7f31b5a491c592361c172946fe7aac
magneticmemoriescz-bit <magnetic.memories.cz@gmail.com>
2025-12-17 20:11:57 +0100

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,9 @@
-
 import React from 'react';
 
 export const Logo: React.FC<{ className?: string }> = ({ className }) => {
     return (
         <img 
-            src="https://i.imgur.com/wMndDuj.png" 
+            src="https://i.imgur.com/b4WFqRi.png" 
             alt="Magnetify.cz" 
             className={`h-16 md:h-24 w-auto object-contain ${className || ''}`} 
         />
